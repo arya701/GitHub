6a8479895a44154a3976c5b32a32dd04d498982b
Arya <arya7012002@gmail.com>
2022-08-28 11:13:07 +0530

    Initial commit

---
+++ checklist.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<conio.h>
+
+class task{
+char task[50];
+
+void change ();
+};
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+
+int main(){
+
+int a,b;
+std::cin >> a>>b;
+std::cout<<a+b;
+getch();
+
+}
